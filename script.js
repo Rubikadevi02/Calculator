ce58fc4d70428f7d06b7be7df42d16d688ab2523
Rubikadevi02 <147690607+Rubikadevi02@users.noreply.github.com>
2023-10-23 18:51:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function goToCalculator() {
+    window.location.href = 'calculator.html';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    const displayValue = document.getElementById('display').value;
+    try {
+        const result = eval(displayValue);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
